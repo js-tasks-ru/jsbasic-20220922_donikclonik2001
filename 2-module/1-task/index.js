e116efcd4c714424c09189b3e0722a5624c7614c
Donier Rakhimov <Rdonik0807@yandex.ru>
2022-10-10 16:19:41 +0300

    1 module/task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
   // ваш код...
+  let arr = Object.values(salaries);
+  return arr.reduce((sum, element) => {
+    if (isFinite(element)) {
+      return sum += element;
+    } else return sum;
+  }, 0);
 }
