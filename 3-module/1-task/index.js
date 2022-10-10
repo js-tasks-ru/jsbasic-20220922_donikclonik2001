8a403b8fb13f1680604a8133b952dd26deea5ffe
Donier Rakhimov <Rdonik0807@yandex.ru>
2022-10-10 19:03:20 +0300

    3 module

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,6 @@
 function namify(users) {
   // ваш код...
+  return users.map(objELement => {
+    return objELement.name;
+  })
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
   // ваш код...
+  let arr = str.split('-');
+
+  let result = arr.map((element, index) => {
+    if (index === 0) {
+      return element;
+    } else {
+      return element[0].toUpperCase() + element.slice(1);
+    }
+  })
+
+  return result.join('');
 }
