f7fb86dfd572c5655b29eeefb8d08ee91f58b0f2
Donier Rakhimov <Rdonik0807@yandex.ru>
2022-10-26 19:19:39 +0300

    module 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,45 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.insertAdjacentHTML('afterbegin', `
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody></tbody>`);
+
+    rows.forEach(row => {
+      this.elem.querySelector('tbody').append(this.createRow(row));
+    })
+  }
+
+  createRow(row) {
+    let tr = document.createElement('tr');
+
+    for (let value of Object.values(row)) {
+      let td = document.createElement('td');
+      td.textContent = value;
+      tr.append(td);
+    }
+
+    let td = document.createElement('td');
+
+    let btn = document.createElement('button');
+    btn.textContent = '[X]';
+    btn.addEventListener('click', function() {
+      btn.closest('tr').remove();
+    })
+    
+    td.append(btn);
+    tr.append(td);
+    return tr;
   }
 }
+
+
